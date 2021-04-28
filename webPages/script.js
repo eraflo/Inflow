49a00365a38464a5195afbe19df62c7047dc8d24
eraflo <florian.maillard2003@gmail.com>
2021-04-28 15:26:20 +0200

    réglage bug

--- webPages/script.js
+++ webPages/script.js
@@ -1,44 +1,40 @@
 $(document).ready(function() {
-	$("#darkTrigger").click(function() {
-	if ($("body").hasClass("darkbody")){
-		$("body").removeClass("darkbody");
-	}
-	else{
-		$("body").addClass("darkbody");
-	}
-	if ($("footer").hasClass("darkfoncé")){
-		$("footer").removeClass("darkfoncé");
-	}
-	else{
-		$("footer").addClass("darkfoncé");
-	}
-	if ($("a").hasClass("darkbody")){
-		$("a").removeClass("darkbody");
-	}
-	else{
-		$("a").addClass("darkbody");
-	}
-	if ($("article").hasClass("darkbody")){
-		$("article").removeClass("darkbody");
-	}
-	else{
-		$("article").addClass("darkbody");
-	}
-	if ($("div").hasClass("darkfoncé")){
-		$("div").removeClass("darkfoncé");
-	}
-	else{
-		$("div").addClass("darkfoncé");
-	}
-});
+    $("#darkTrigger").click(function() {
+        if ($("body").hasClass("darkbody")) {
+            $("body").removeClass("darkbody");
+        } else {
+            $("body").addClass("darkbody");
+        }
+        if ($("footer").hasClass("darkfoncé")) {
+            $("footer").removeClass("darkfoncé");
+        } else {
+            $("footer").addClass("darkfoncé");
+        }
+
+        if ($("article").hasClass("darkbody")) {
+            $("article").removeClass("darkbody");
+        } else {
+            $("article").addClass("darkbody");
+        }
+        if ($("div").hasClass("darkfoncé")) {
+            $("div").removeClass("darkfoncé");
+        } else {
+            $("div").addClass("darkfoncé");
+        }
+        if ($("header").hasClass("darkfoncé")) {
+            $("header").removeClass("darkfoncé");
+        } else {
+            $("header").addClass("darkfoncé");
+        }
+    });
 })
 
 
-$(document).ready(function () {
-	var d = new Date();
-	var n = d.getHours();
+$(document).ready(function() {
+    var d = new Date();
+    var n = d.getHours();
 
-	if(n > 18 || n < 8){
-		$("body").addClass("dark");
-	}
+    if (n > 18 || n < 8) {
+        $("body").addClass("dark");
+    }
 });
