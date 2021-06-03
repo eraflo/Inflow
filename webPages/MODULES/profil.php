f585e582505aa6429947d318132ff284fc4a1790
eraflo <florian.maillard2003@gmail.com>
2021-06-03 21:21:26 +0200

    update supprimer bio dans module

--- webPages/MODULES/profil.php
+++ webPages/MODULES/profil.php
@@ -12,7 +12,6 @@ if(isset($_SESSION['id']) && ($_SESSION['id'] > 0)) {
         <img src="membres/avatars/<?php echo $M_userinfos['avatar']; ?>" class="avatar" style="max-width:7em;">
     <?php } ?>
     <b><a href="Profil.php?id=<?= $M_userinfos['id'] ?>"><?php echo $M_userinfos['pseudo']; ?></a></b>
-    <i><span><?php echo $M_userinfos['biographie']; ?></span></i><br/>
     <a href="Déconnexion.php"><div class="PActions">Se déconnecter</div></a>
 </div>
 <?php } ?>
