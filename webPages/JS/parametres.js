8a6bade6ce73f6e75a3f62a613415456b6057818
Anyr Bagui <53833195+Anyr-Bagui@users.noreply.github.com>
2021-06-03 23:43:18 +0200

    Synchroniser les paramètres sur la db
    Seulement la police d'écriture pour l'instant

--- webPages/JS/parametres.js
+++ webPages/JS/parametres.js
@@ -5,3 +5,20 @@ function change_font(font) {
         window.localStorage["articles_font"] = font;
     }
 }
+
+function get_settings() {
+    // (font, user-color-mode)
+    l = window.localStorage;
+    return (l["articles_font"], l["user-color-mode"]);
+}
+
+$(document).ready(function () {
+    // Synchroniser les paramètres avec les paramètres sur le serveur
+    settings = get_settings();
+    if (settings[0] != font && font != 0) {
+        window.localStorage["articles_font"] = font;
+    }
+    if (settings[1] != theme && theme != 0) {
+        window.localStorage["user-color-mode"] = theme;
+    }
+});
--- webPages/Parametres.php
+++ webPages/Parametres.php
@@ -3,20 +3,59 @@
 session_start();
 $bdd = new PDO("mysql:host=127.0.0.1;dbname=inflow;charset=utf8", "root", "");
 
+$requser = $bdd->prepare('SELECT * FROM membres WHERE id = ?');
+$requser->execute(array($_SESSION['id']));
+$userinfos = $requser->fetch();
 
+// Rechercher les informations de la base de données
+$reqsettings = $bdd->prepare('SELECT * FROM membres WHERE id = ?');
+$reqsettings->execute(array($_SESSION['id']));
+$settings = $reqsettings->fetch();
 
+$reqfont = $bdd->prepare('SELECT nom_police FROM police_ecriture WHERE id_police = ?');
+$reqfont->execute(array($settings['font']));
+$font = $reqfont->fetch();
+
+$reqtheme = $bdd->prepare('SELECT nom_theme FROM themes_couleurs WHERE id_theme = ?');
+$reqtheme->execute(array($settings['user_color_mode']));
+$theme = $reqtheme->fetch();
+
+$CHANGE_JS = false;
 if(isset($_POST['police'])) {
-    // Importer le script avant de l'excecuter
-    echo '<script src="JS/parametres.js"></script>';
-    echo '<script type="text/javascript">',
-        'change_font("'.$_POST['police'].'");',
-        '</script>';
+    $CHANGE_JS = true;
+    // Insérer dans la base de données
+    $reqfontname = $bdd->prepare('SELECT id_police FROM police_ecriture WHERE nom_police = ?');
+    $reqfontname->execute(array($_POST['polices']));
+    $fontname = $reqfontname->fetch();
+    $insertPolice = $bdd->prepare("UPDATE membres SET font = ? WHERE id = ?");
+    $insertPolice->execute(array($fontname[0], $_SESSION['id']));
+
+    header("Location: Parametres.php");
+}
+
+if (isset($font) && !empty($font)){
+    echo '<script>font = "'.$font[0].'";</script>';
+} else {
+    echo '<script>font = 0;</script>';
+}
+if (isset($theme) && !empty($theme)){
+    echo '<script>theme = "'.$theme[0].'";</script>';
+} else {
+    echo '<script>theme = 0;</script>';
 }
 
 include 'tmpl_top.php';
 ?>
 <script src="JS/parametres.js"></script>
 
+<?php 
+if ($CHANGE_JS) {
+// Changer la variable locales
+echo '<script src="JS/parametres.js"></script>';
+echo '<script type="text/javascript">',
+    'change_font("'.$_POST['polices'].'");',
+    '</script>';
+} ?>
 <?php
 include 'MODULES/begin_left.php';
 include 'MODULES/profil.php';
@@ -26,13 +65,13 @@ include 'MODULES/end.php';
 <div class="middle">
     <form method="POST" enctype="multipart/form-data">
         Police des articles : 
-        <select type="text" name="police" style="min-width:10em;max-height:fit-content;">
+        <select type="text" name="polices" style="min-width:10em;max-height:fit-content;">
             <option value="PT Serif" style="font-family:'Pt Serif';">PT Serif</option>
             <option value="Lato" style="font-family:'Lato';">Lato</option>
             <option value="Kalam" style="font-family:'Kalam';">Kalam</option>
             <option value="Roboto" style="font-family:'Roboto';">Roboto</option>
         </select><br/><br/>
-        <input type="submit" value="Modifier"/>
+        <input type="submit" name="police" value="Modifier"/>
     </form>
     <br/>
     <h1 align="center">Dark Mode :</h1>
