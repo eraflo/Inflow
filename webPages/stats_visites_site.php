6f1686e1425f12dc9dac90b63270e9c0d0f3013a
Anyr Bagui <53833195+Anyr-Bagui@users.noreply.github.com>
2021-06-04 23:02:25 +0200

    Fix https://github.com/eraflo/Inflow/issues/134 Compteur de visites

---
+++ webPages/stats_visites_site.php
@@ -0,0 +1,22 @@
+<?php
+if(file_exists('compteur_visites.txt'))
+{
+    $M_compteur_f = fopen('compteur_visites.txt', 'r+');
+    $M_compte = fgets($M_compteur_f);
+}
+else
+{
+    $M_compteur_f = fopen('compteur_visites.txt', 'a+');
+    $M_compte = 0;
+}
+echo $_COOKIE["last_visit"];
+if (!isset($_COOKIE["last_visit"])) {
+    echo "eeeeeeeeeeeeee";
+    setcookie("last_visit", time(), time()+3600, '/', null, false, true); // expire en 1h
+    $M_compte++;
+    fseek($M_compteur_f, 0);
+    fputs($M_compteur_f, $M_compte);
+}
+
+fclose($M_compteur_f);
+?>
