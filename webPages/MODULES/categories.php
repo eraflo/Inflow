a315165417adea1f655851c45c01d7e4fb7b6ee6
Enzo-py <85191552+Enzo-py@users.noreply.github.com>
2021-06-07 23:46:44 +0200

    CSS pour les catégories

--- webPages/MODULES/categories.php
+++ webPages/MODULES/categories.php
@@ -5,8 +5,8 @@
 $M_categories = $bdd->query('SELECT * FROM categories');
 ?>
 
-<div class="module">
+<div class="module" style="border-top: solid;border-color: var(--color-text);border-width: 0.5px;width: 300px;">
     <?php while($c = $M_categories->fetch()) { ?>
-        <a href="tmpl_categories.php?id=<?= $c['id'] ?>" class=""><?= $c['nom'] ?></a>
+        <a href="tmpl_categories.php?id=<?= $c['id'] ?>" class="" > <b class="PTitle">Catégories:</b><br /><i><?= $c['nom'] ?></i></a>
     <?php } ?>
 </div>
