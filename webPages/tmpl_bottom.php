7a6c80c4a615116971ee7f1d269fa35cb154245f
Anyr Bagui <53833195+Anyr-Bagui@users.noreply.github.com>
2021-07-04 00:15:07 +0200

    Placement alerte cookie #230

--- webPages/tmpl_bottom.php
+++ webPages/tmpl_bottom.php
@@ -1,4 +1,10 @@
             <footer class="footer container">
+                <?php if(!isset($_COOKIE['accept_cookie'])) { ?>
+                    <div class="element cookie-alert">En poursuivant votre navigation sur ce site, vous acceptez l'utilisation de cookies pour vous 
+                        proposez des contenus et services adaptés à vos centres d'interêt.<br/>
+                        <a class="cookie-alert-ok-button" href="accept_cookie.php">OK</a>
+                    </div>
+                <?php } ?>
                 <div style="flex:1;"><p class="element"> Articles et actus sur le rap - <i>Axelito à l'origine</i> - <a class="can_rainbow" href="https://discord.com/channels/826440352810139678/826491405856800788">Rejoignez le Discord !</a></p></div>
             </footer>
         </div>
