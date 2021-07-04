7dab2be5e7ae140b39d72a24d2b33d32f4607132
timo-thi <85194871+timo-thi@users.noreply.github.com>
2021-07-04 19:35:05 +0200

    Update webp_converter.py
    Correction

--- webPages/webp_converter.py
+++ webPages/webp_converter.py
@@ -8,11 +8,9 @@ def converter(in_file, out_file):
     if out_file == None :
         out_file = path.basename(in_file)
         out_file = out_file[:out_file.index(".")] + ".webp"
-    print(cwebp(input_image=in_file, output_image=out_file, option="-q 60", logging="-v"))
-    return
+    print(cwebp(input_image = in_file, output_image = file_path + out_file, option = "-q 60", logging = "-v"))
 
 args = sys.argv
-print(len(args), args)
 
 if len(args) != 3 :
     print("wrong arguments : webp_converter [in_file] [out_file] \n\t [obliged] (optional)")
