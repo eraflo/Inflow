b801d007a28df4cacb7ba57592ec616cbfa832d2
timo-thi <85194871+timo-thi@users.noreply.github.com>
2021-07-04 18:24:39 +0200

    Update webp_converter.py

--- webPages/webp_converter.py
+++ webPages/webp_converter.py
@@ -1,10 +1,20 @@
-import os.path
+import os.path as path
+import sys
 from webptools import cwebp
 
-in_file = input("enter your file : ")
-path = os.path.dirname(in_file) + "\\"
-extension = in_file[in_file.index("."):]        
-out_file = os.path.basename(in_file)
-out_file = out_file[:out_file.index(".")] + ".webp"
+def converter(in_file, out_file):
+    file_path = path.dirname(in_file) + "\\"
+    extension = in_file[in_file.index("."):]
+    if out_file == None :
+        out_file = path.basename(in_file)
+        out_file = out_file[:out_file.index(".")] + ".webp"
+    print(cwebp(input_image=in_file, output_image=out_file, option="-q 60", logging="-v"))
+    return
 
-print(cwebp(input_image=in_file, output_image=path+out_file, option="-q 60", logging="-v"))
+args = sys.argv
+print(len(args), args)
+
+if len(args) != 3 :
+    print("wrong arguments : webp_converter [in_file] [out_file] \n\t [obliged] (optional)")
+else:
+    converter(args[1], args[2])
