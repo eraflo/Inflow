c8e50f4a5d5a6e777daaee37b57f870d52e9af8d
Anyr Bagui <53833195+Anyr-Bagui@users.noreply.github.com>
2021-08-07 00:47:03 +0200

    Style page Poster + Fix #223
    Style du formulaire page poster et page modification article

--- webPages/Modification_Articles_Categories.php
+++ webPages/Modification_Articles_Categories.php
@@ -26,38 +26,40 @@ include 'MODULES/end.php';
     <script src="JS/categories.js" defer></script>
 </head>
 <div class="middle">
-    <article id="modification_article">
-        <!--Formulaire pour modifier l'article-->
-        <form method="POST" enctype="multipart/form-data">
-            <input type="text" name="article_titre" value="<?= $article['titre'] ?>" /> <br/>
-            <select type="text" name="article_id_auteur">
+    <article class="ProfilTxt" id="modification_article">
+        <!--Formulaire pour postez des articles-->
+        <form method="POST" enctype="multipart/form-data" class="con_ins">
+            <input class="input_form" type="text" name="article_titre" value="<?= $article['titre'] ?>" />
+            <select class="input_form" type="text" name="article_id_auteur" placeholder="Auteur">
                 <option value="<?= $article['id_auteur'] ?>" style="font-style:italic;"><?= $article['auteur'] ?></option>
                 <?php while($a = $auteurs->fetch()) { ?>
                     <option value="<?= $a['id'] ?>"><?= $a['pseudo'] ?></option>
                 <?php } ?>
             </select><br/>
-            <select id="categorie_selection" type="text" name="article_id_categorie">
-                <?php if($article_categorie) { ?><option value="<?= $article['id_categories'] ?>" style="font-style:italic;"><?= $article_categorie ?></option><?php } ?>
-                <option value="" style="font-style:italic;">Aucune catégorie</option>
-                <option value="Nouvelle" style="font-weight:bold;">Nouvelle catégorie</option>
+            <select class="input_form" id="categorie_selection" type="text" name="article_id_categorie">
+                <?php if($article_categorie) { ?>
+                    <option value="<?= $article['id_categories'] ?>" style="font-style:italic;"><?= $article_categorie ?></option><?php } ?>
+                    <option value="" style="font-style:italic;">Aucune catégorie</option>
+                    <option value="Nouvelle" style="font-weight:bold;">Nouvelle catégorie</option>
                 <?php while($c = $categories->fetch()) { ?>
                     <option value="<?= $c['id'] ?>"><?= $c['nom'] ?></option>
                 <?php } ?>
-            </select>
-            <input id="categorie_name" type="text" name="article_nom_categorie" placeholder="Nom de la catégorie" />
-            <input id="categorie_desc" type="text" name="article_desc_categorie" placeholder="Description de la catégorie" /><br/>
-            <textarea type="text" name="article_comment" style="width:100%;resize:vertical;"><?= $article['descriptions'] ?></textarea><br/>
-            <textarea id="editor" name="article_contenu"><?php if(isset($contenu)){echo $contenu;} ?></textarea><br/>
-            <input type="file" name="miniature"/><br/>
-            <input type="submit" value="Modifier l'article" /><br />
+            </select><br/>
+            <input class="input_form" id="categorie_name" type="text" name="article_nom_categorie" placeholder="Nom de la catégorie" />
+            <input class="input_form" id="categorie_desc" type="text" name="article_desc_categorie" placeholder="Description de la catégorie" /><br/>
+            <textarea class="input_form" type="text" name="article_comment" placeholder="Description" style="resize:vertical;width:100%"><?= $article['descriptions'] ?></textarea><br/>
+            <div style="text-align:initial;">
+                <textarea class="input_form" id="editor" name="article_contenu" placeholder="Contenu de l'article">
+                    <?php if(isset($contenu)){echo str_replace(array("<br />", "<br/>", "<br >", "<br>"), '', $contenu);} ?>
+                </textarea>
+            </div><br/>
+            <label class="Titre_form" for="miniature">Miniature : </label><input class="input_form" type="file" name="miniature"/><br/>
+            <input class="input_form" type="submit" value="Modifier l'article" /><br />
         </form>
         <br />
         <!--Affiche message en lien avec le transfert des données du formulaire.
             Ex : 1 des champs n'est pas remplie -> Affiche "Erreur"; Si tout est bien remplie, affiche "Votre article a été posté"-->
         <?php if(isset($message)) { echo $message; } ?>
         <br />
-    </article>
-    <article id="modification_categorie">
-    <!--Formulaire pour modifier la catégorie-->
     </article>
 </div>
