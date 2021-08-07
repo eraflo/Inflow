222f33c50328a4bd502f478057962a2de35b89c3
eraflo <florian.maillard2003@gmail.com>
2021-08-07 18:09:55 +0200

    supprimer compte par admin + bug local storage résolu

--- webPages/Deconnexion.php
+++ webPages/Deconnexion.php
@@ -2,5 +2,9 @@
 session_start();
 $_SESSION = array();
 session_destroy();
-header("Location: Connexion.php");
 ?>
+
+<script type="text/javascript">
+    window.localStorage.clear();
+    window.location="Connexion.php";
+</script>
